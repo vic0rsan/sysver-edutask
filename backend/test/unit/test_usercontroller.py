fc4edcbf7b05cb6df028a5dc7c1bf4dac8afa290
gusu20 <gusu20@student.bth.se>
2022-04-20 19:35:41 +0200

    Add test for invalid email

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -9,30 +9,22 @@ def sut(email: str, usersLen: int):
     users = []
     for i in range(usersLen):
         users.append({'email': email})
+    if len(users) == 0:
+        users = [None]
     mockedUserController.find.return_value = users
     mockedSut = UserController(dao=mockedUserController)
     return mockedSut
 
 @pytest.mark.demo
-@pytest.mark.parametrize('email, expected, usersLen', [('jane.doe@gmail.com', {'email': 'jane.doe@gmail.com'}, 1)])
-def test_valid_email_exists(sut, expected):
+@pytest.mark.parametrize('email, expected, usersLen', [('jane.doe@gmail.com', {'email': 'jane.doe@gmail.com'}, 1), ('jane.doe@gmail.com', {'email': 'jane.doe@gmail.com'}, 2), ('jane.doe@gmail.com', None, 0)])
+def test_get_user_by_email(sut, expected):
     email = "jane.doe@gmail.com"
     emailRes = sut.get_user_by_email(email)
     assert emailRes == expected
 
 @pytest.mark.demo
-@pytest.mark.parametrize('email, usersLen', [('jane.doe.gmail.com', 1)])
+@pytest.mark.parametrize('email, usersLen', [('jane.doe.gmail.com', 0)])
 def test_invalid_email(sut):
-    """ Test invalid email by replacing '@' with '.'"""
     email = "jane.doe.gmail.com"
-
     with pytest.raises(ValueError):
         sut.get_user_by_email(email)
-
-@pytest.mark.demo
-@pytest.mark.parametrize('email, expected, usersLen', [('jane.doe@gmail.com', {'email': 'jane.doe@gmail.com'}, 1), ('jane.doe@gmail.com', {'email': 'jane.doe@gmail.com'}, 2)])
-def test_duplicate_email(sut, expected):
-    """ Test duplicate email"""
-    email = "jane.doe@gmail.com"
-    emailRes = sut.get_user_by_email(email)
-    assert emailRes == expected
